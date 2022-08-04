d4e03c40dba1a1df18415dc3a66a4141b61097e1
EPW <101692532+EPW80@users.noreply.github.com>
2022-08-04 00:57:40 -0700

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x62815c3426a4592ce7980fDf614606B0DE807310'
+  'your contract number'
 );
 
 export default instance;
